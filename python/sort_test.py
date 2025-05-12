b951916f6663eb43fa0634f91f5af4b9955703d4
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2025-05-12 22:25:50 +0200

    clean up code a bit

--- python/sort_test.py
+++ python/sort_test.py
@@ -1,5 +1,8 @@
 import os
+from typing import Callable
+
 import sort_execute
+from Element import Element
 
 
 test_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'testdata')
@@ -10,6 +13,7 @@ def test(algorithm: str) -> None:
     @param algorithm the sorting algorithm to test.
     """
     print(f'testing {algorithm}...')
+    sort = sort_execute.sort_functions[algorithm]
 
     # go through all test data set stages
     for data_stage in range(1, 6):
@@ -25,17 +29,22 @@ def test(algorithm: str) -> None:
             assert len(output_list) == len(input_list)
         except Exception:
             assert False, f'Cannot test {input_name} because output data is missing (or incorrect)'
-        # check all test data sets
-        fail_count = 0
-        for test_id, (input_data, expected) in enumerate(zip(input_list, output_list), 1):
-            sort = sort_execute.sort_functions[algorithm]
-            output, _, count_str = sort_execute.execute(sort, input_data)
-            print(f"    tested case {test_id} {shorten(input_data)} in {count_str}")
-            if output != expected:
-                print(f"FAILED.\noutput   {output} !=\nexpected {expected}")
-                fail_count += 1
-        # abort if a test case failed
-        assert fail_count == 0, f"{fail_count} test cases failed for stage {data_stage}."
+        test_datasets(sort, input_list, output_list)
+
+def test_datasets(
+    sort: Callable[[list[Element]], None],
+    input_list: list[list[int]],
+    output_list: list[list[int]]
+):
+    fail_count = 0
+    for test_id, (input_data, expected) in enumerate(zip(input_list, output_list), 1):
+        output, _, count_str = sort_execute.execute(sort, input_data)
+        print(f"    tested case {test_id} {shorten(input_data)} in {count_str}")
+        if output != expected:
+            print(f"FAILED.\noutput   {output} !=\nexpected {expected}")
+            fail_count += 1
+    # abort if a test case failed
+    assert fail_count == 0, f"{fail_count} test cases failed for stage {data_stage}."
 
 class Threedots:
     def __repr__(self):
