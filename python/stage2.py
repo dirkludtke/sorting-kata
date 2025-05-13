6256cbaba2c04e6024391bb51864773070633d1f
Dirk Luedtke <96847252+dirkludtke@users.noreply.github.com>
2025-05-13 13:52:41 +0200

    harmonize the py documentation with ts

--- python/stage2.py
+++ python/stage2.py
@@ -6,6 +6,6 @@ def sort(elements: list[Element]):
     sort a list of elements in ascending order with O(n * log(n)) time complexity.
     @param elements: list of elements to sort (sorting will modify the list).
     """
-    # this is a good time to use quicksort. please use the first or last element as the pivot.
-    # this is for didactic purposes as we want to explore where Quicksort is not so quick.
+    # this is a good time for quicksort. please use the first or last element as the pivot.
+    # this is for didactic purposes as we want to explore where quicksort is not so quick.
     pass
--- python/stage4.py
+++ python/stage4.py
@@ -6,4 +6,5 @@ def sort(elements: list[Element]):
     sort a list of elements in ascending order with O(n) maximum time complexity.
     @param elements: list of elements to sort (sorting will modify the list).
     """
-    # yeah. you can do it. the answer is in the documentation of the Element class.
+    # yeah. you can do it. there is a hint in the documentation of the Element class.
+    pass
