ddc2795248c18d05c12e9e0330acf37302d5b0c4
Dirk Luedtke <96847252+dirkludtke@users.noreply.github.com>
2025-05-16 13:07:29 +0200

    correct type in instructions of java and ts version. python was fine

--- java/Stage4.java
+++ java/Stage4.java
@@ -6,7 +6,7 @@ class Stage4 implements Stage {
      */
     @Override
     public void sort(Element[] elements) {
-        // yeah. you can do it. the is a hint in the documentation of the Element class.
+        // yeah. you can do it. there is a hint in the documentation of the Element class.
     }
 
 }
--- typescript/stage4.ts
+++ typescript/stage4.ts
@@ -6,7 +6,7 @@ import type { ListElement } from "./Element";
  * @param elements list of elements to sort (sorting will modify the list).
  */
 function sort(elements: ListElement[]): void {
-  // yeah. you can do it. the is a hint in the documentation of the Element class.
+  // yeah. you can do it. there is a hint in the documentation of the Element class.
 }
 
 
