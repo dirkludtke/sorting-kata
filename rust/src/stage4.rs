89d11eb56f4fedee7bb1eae43e42e40f22409be6
Dirk Luedtke <96847252+dirkludtke@users.noreply.github.com>
2025-05-22 17:54:20 +0200

    stage4 implementation for Rust

--- rust/src/stage4.rs
+++ rust/src/stage4.rs
@@ -1,10 +1,30 @@
 use crate::element::Element;
 
+const SIZE: usize = 1 << 16; // 2^16 = 65536
 
 /// sort a vector of elements in ascending order with O(n) maximum time complexity.
 ///
 /// # Arguments
 /// * `elements` - elements to sort (sorting will modify the vector).
-pub fn sort(_elements: &mut Vec<Element>) {
+pub fn sort(elements: &mut Vec<Element>) {
     // yeah. you can do it. there is a hint in the documentation of the Element struct.
+    let mut sorted_elements = vec![Vec::new(); SIZE];
+    loop {
+        match elements.pop() {
+            None => break,
+            Some(element) => {
+                sorted_elements[element.get_value() as usize].push(element);
+            }
+        }
+    }
+    for mut equal_elements in sorted_elements {
+        loop {
+            match equal_elements.pop() {
+                None => break,
+                Some(element) => {
+                    elements.push(element);
+                }
+            }
+        }
+    }
 }
