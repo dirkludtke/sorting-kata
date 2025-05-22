d9a2eb5fc1f4929d2fc64aef2ffe0fff505671b5
Dirk Luedtke <96847252+dirkludtke@users.noreply.github.com>
2025-05-22 15:48:54 +0200

    stage1 implementation for Rust

--- rust/src/stage1.rs
+++ rust/src/stage1.rs
@@ -5,7 +5,15 @@ use crate::element::Element;
 ///
 /// # Arguments
 /// * `elements` - elements to sort (sorting will modify the vector).
-pub fn sort(_elements: &mut Vec<Element>) {
+pub fn sort(elements: &mut Vec<Element>) {
     // even if you know faster algorithms, here, we want the simplest solution possible.
     // something like bubble sort or insertion sort is fine. maximum 6 lines of code.
+    let n = elements.len();
+    for i in 0..n {
+        for j in 0..n - i - 1 {
+            if elements[j] > elements[j + 1] {
+                elements.swap(j, j + 1);
+            }
+        }
+    }
 }
