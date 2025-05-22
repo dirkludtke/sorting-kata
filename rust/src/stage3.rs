e04c7036f2af3cb1619356fd4a26f87f1597909a
Dirk Luedtke <96847252+dirkludtke@users.noreply.github.com>
2025-05-22 17:33:33 +0200

    stage3 implementation for Rust

--- rust/src/stage3.rs
+++ rust/src/stage3.rs
@@ -5,6 +5,25 @@ use crate::element::Element;
 ///
 /// # Arguments
 /// * `elements` - elements to sort (sorting will modify the vector).
-pub fn sort(_elements: &mut Vec<Element>) {
+pub fn sort(elements: &mut Vec<Element>) {
     // quicksort has a worst case of O(n**2). let's go with something like mergesort.
+    if elements.len() <= 1 { 
+        return;
+    }
+    let mid = elements.len() / 2;
+    let mut left = elements[0..mid].to_vec();
+    let mut right = elements[mid..].to_vec();
+    sort(&mut left);
+    sort(&mut right);
+    for i in 0..elements.len() {
+        if left.is_empty() {
+            elements[i] = right.remove(0);
+        } else if right.is_empty() {
+            elements[i] = left.remove(0);
+        } else if left[0] < right[0] {
+            elements[i] = left.remove(0);
+        } else {
+            elements[i] = right.remove(0);
+        }
+    }
 }
